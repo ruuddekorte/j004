6ba35e1ce4f7167f83558b224e394b5ed9f93365
simplon <dekorte.tsrit@gmail.com>
2017-11-16 11:16:14 +0100

    a fresh start

---
+++ js/app.js
@@ -0,0 +1,29 @@
+console.log("hello world");
+
+function add (a,b){
+	return a+b;
+};
+
+function sou (a,b){
+	return a-b;
+};
+
+function div (a,b){
+	if (b!==) {
+		return a+b;
+	}else{
+		return "error 'division by zero'"
+	}
+};
+
+function mul (a,b){
+	return a*b;
+};
+
+console.log (add(15,25));
+
+console.log (sou(15,25));
+
+console.log (div(15,25));
+
+console.log (mul(15,25));
